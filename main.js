e01dfde38983b47848198e24ccbcbabf01aa5c40
stevula <brodericksteven@gmail.com>
2019-03-09 16:56:29 -0800

    Enable sending push from server

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then((registration) => {
-    registration.active.postMessage('Message from main.js');
-    Notification.requestPermission();
-  }).catch(console.log);
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(async (registration) => {
+      const PUBLIC_KEY = 'BF7lT0sfoVzOq__qQFe8CipAV2CHu7PoW1766rI7dho1oSaB_a7YlAYplRlMuZT_rmAomevKy2JyQ5FQuQbLsrI';
+
+      const subscription = await registration.pushManager.getSubscription();
+      if (subscription) {
+        return subscription.toJSON();
+      }
+
+      const applicationServerKey = urlBase64ToUint8Array(PUBLIC_KEY);
+
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey,
+      });
+    })
+    .then(console.log)
+    .catch(console.log);
 }
--- pushServer.js
+++ pushServer.js
@@ -1,1 +1,21 @@
 const webpush = require('web-push');
+const vapid = require('./vapid.json');
+
+webpush.setVapidDetails(
+  'mailto:brodericksteven@gmail.com',
+  vapid.publicKey,
+  vapid.privateKey,
+);
+
+const pushSubscription = {
+  endpoint:
+    'https://fcm.googleapis.com/fcm/send/fjWTJM8NOF4:APA91bGqqnqUeGkin9Z6p_9Z0VZFLXjZ8vfgktLsoCoL6M4uimzp3I3Nw6hMJDd3jfQ9OQ_m87zYF6sQkJy3I59KKrTpIhpD_V6S3kNQzfU2hIaE5W0NX0x5B0Q7Q2Ggorlx0-RtFGxU',
+  keys: {
+    auth: '0YpMcczMpndUMCu5tLzokQ',
+    p256dh:
+      'BMaBQ7aRH48OJi_mfIuVKjSWgwe2rLJf8oaGajS52DT-P5zYmoNbYOMQX5c0Wr4G11In4xwr3yACdv6cTYh6IOE',
+  },
+};
+
+webpush.sendNotification(pushSubscription, 'Notification from push server');
+console.log('Push sent to client');
